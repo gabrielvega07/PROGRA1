c26499bb8f09b4aeb30934faa2d5d8db44028fa5
gabrielvega07 <79347680+gabrielvega07@users.noreply.github.com>
2021-03-05 22:38:46 -0600

    Add files via upload

---
+++ Adivinar fecha de cumple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Adivinar_fecha_de_cumple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d = 0, m = 0, resultado;
+            int pos0, pos1, pos2, pos3;
+
+
+
+            Console.WriteLine("Ingrese su dia de nacimiento");
+            d = Convert.ToInt32(Console.ReadLine());
+            d = ((d * 20) + 73) * 5;
+            Console.WriteLine("Ingrese su mes de nacimiento: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            resultado = d + m - 365;
+
+
+            
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+            
+            
+            
+            pos0 = (resultado % 10000) / 1000;
+            pos1 = (resultado % 10000) % 1000 / 100;
+            pos2 = ((resultado % 10000) % 1000) % 100 / 10;
+            pos3 = (((resultado % 10000) % 1000) % 100) % 10;
+            Console.WriteLine("Muy bien el dia de tu cumpleaños es: " + pos0 + pos1 + " y el  mes de tu cumpleaños es:" + pos2 + pos3);
+            
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+
+            Console.WriteLine("Piense en un numero");
+            Console.ReadKey();
+            Console.WriteLine("Multiplicar por 2");
+            Console.ReadKey();
+            Console.WriteLine("Sumele 8");
+            Console.ReadKey();
+            Console.WriteLine("Multiplicar por 5");
+            Console.ReadKey();
+
+            Console.Write("Ingrese el resultado: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            numero = numero / 10;
+            resultado = numero - 4;
+
+            Console.WriteLine("Su numero es: " + resultado);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
