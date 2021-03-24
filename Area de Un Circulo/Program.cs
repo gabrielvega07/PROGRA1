ad382c42715b0bef815ed4e84ced6cec480213b9
gabrielvega07 <79347680+gabrielvega07@users.noreply.github.com>
2021-03-23 20:05:46 -0600

    Add files via upload

---
+++ Area de Un Circulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Area_de_Un_Circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.1416, area, radio;
+             
+
+            Console.Write("Ingrese el radio del circulo:");
+            radio = Convert.ToInt32(Console.ReadLine());
+
+            area = pi * (radio 2);
+
+            Console.WriteLine("el area es:" + area);
+        }
+    }
+}
